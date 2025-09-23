021236e6430f0a91d1bb30b29b05f0cfb869f43c
Andrea Prause <andreaprause@MacBook-de-Andrea.local>
2025-09-23 20:10:20 -0300

    Modificación de fuente tipográfica, precisión de CSS y añadido de JavaScript

---
+++ contact.js
@@ -0,0 +1,56 @@
+// lista de tareas con tareas prearmadas para conectar con la artista //
+
+const input = document.getElementById("new");
+const button = document.getElementById("addBtn");
+const list = document.getElementById("list");
+
+const ourList = [
+    { task: "Follow on Instagram" },
+    { task: "Inquiry about an artwork" },
+    { task: "Ask about commissions" },
+]
+
+ourList.forEach(suggested => {
+    const li = document.createElement("li")
+    li.textContent = suggested.task
+
+    const btnDelete = document.createElement("button")
+    btnDelete.textContent = "☒"
+    btnDelete.addEventListener("click", () => li.remove())
+    li.appendChild(btnDelete)
+
+    const btnCompleted = document.createElement("button")
+    btnCompleted.textContent = "☑"
+    btnCompleted.addEventListener("click", (e) => {
+        e.preventDefault();
+        li.style.color = "green"
+    })
+    li.appendChild(btnCompleted)
+
+    list.appendChild(li)
+
+})
+
+button.addEventListener("click", (e) => {
+    e.preventDefault();
+    const text = input.value;
+    if (text) {
+        const li = document.createElement("li")
+        li.textContent = text
+
+        const btnDelete = document.createElement("button")
+        btnDelete.textContent = "☒"
+        btnDelete.addEventListener("click", () => li.remove())
+        li.appendChild(btnDelete)
+
+        const btnCompleted = document.createElement("button")
+        btnCompleted.textContent = "☑"
+        btnCompleted.addEventListener("click", (e) => {
+            e.preventDefault();
+            li.style.color = "green"
+        })
+        li.appendChild(btnCompleted)
+
+        list.appendChild(li)
+    }
+})
